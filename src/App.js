993327b323d4ac3e14c7c743e68c2e1e60823701
Dylan Russo <dylan.a.russo@gmail.com>
2021-03-16 21:34:41 -0400

    Add aboutus page and change tab routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 import Todo from "./todo";
 import Contact from "./contact";
+import AboutUs from "./aboutus";
 
 function Home() {
   return(
@@ -24,27 +25,27 @@ function App() {
         <nav>
           <ul className={'w-full'}>
             <li className={'inline-block w-1/3 text-center p-5 bg-blue-500 text-white'}>
-              <NavLink to="/" activeClassName="font-bold" exact={true}>Home</NavLink>
+              <NavLink to="/" activeClassName="font-bold" exact={true}>Todo</NavLink>
             </li>
             <li className={'inline-block w-1/3 text-center border-r border-blue-800 border-l p-5 bg-blue-500 text-white'}>
-              <NavLink to="/todos" activeClassName="font-bold" exact={true}>Todos</NavLink>
+              <NavLink to="/contact" activeClassName="font-bold" exact={true}>Contact Us</NavLink>
             </li>
             <li className={'inline-block w-1/3 text-center p-5 bg-blue-500 text-white'}>
-              <NavLink to="/contact" activeClassName="font-bold" exact={true}>Contact</NavLink>
+              <NavLink to="/aboutus" activeClassName="font-bold" exact={true}>About Us</NavLink>
             </li>
           </ul>
         </nav>
 
 
         <Switch>
-          <Route path="/todos">
-            <Todo />
-          </Route>
           <Route path="/contact">
             <Contact />
           </Route>
+          <Route path="/AboutUs">
+            <AboutUs />
+          </Route>
           <Route path="/">
-            <Home />
+            <Todo />
           </Route>
         </Switch>
       </div>
---
+++ src/aboutus.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+function AboutUs(props) {
+    return (
+        <div>
+
+            <h1>About Us page</h1>
+        </div>
+    );
+}
+
+export default AboutUs;
