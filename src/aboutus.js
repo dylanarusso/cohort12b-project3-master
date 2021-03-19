10e57583d9a47c904f81ca59f95a5e1a75eb00ce
Dylan Russo <dylan.a.russo@gmail.com>
2021-03-18 20:55:09 -0400

    Updated AboutUs Style

--- src/aboutus.js
+++ src/aboutus.js
@@ -7,8 +7,8 @@ function AboutUs(props) {
     
     return (
         <div>
-                <section style={{fontSize:"30px", flexWrap:"wrap"}}>
-                    <p style={{margin: "25px"}}>
+                <section style={{fontSize:"30px", flexWrap:"wrap", backgroundColor:"rgba(59, 130, 246)", color:"white"}}>
+                    <p style={{marginTop: "100px"}}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
 
